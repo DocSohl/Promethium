f5632e0b15d370f0330bd6ce35d8bc768bede291
Ian Sohl <docsohl@gmail.com>
2013-03-03 01:10:55 -0700

    Fixed Startup Script

--- Assets/Scripts/startup.js
+++ Assets/Scripts/startup.js
@@ -2,19 +2,19 @@
 import System.IO;
 
 var station : GameObject;
-var map = "\\Resources\\coordinate.txt";
+var map = "\\Resources\\coordinateVersion Three.txt";
 
 function Start () {
 	//Test creation of a single testing station
 	var positions = ReadMap(map);
 	for (position in positions) {
-		createStation(position.Split(","[0]));
+		if (position!=""){
+			createStation(position.Split(","[0]));
+		}
 	}
-	//createStation(position);
 }
 
 function createStation (position : System.String[]) {
-	Debug.Log(position[0]);
 	var x = parseInt(position[0].Replace("\r",""));
 	var y = parseInt(position[1].Replace("\r",""));
 	var z = parseInt(position[2].Replace("\r",""));
