6056c13684c971a4cce6ad27e0c13c3155659a2f
Ian Sohl <docsohl@gmail.com>
2013-03-03 00:35:23 -0700

    Changed Startup to load from file

--- Assets/Scripts/startup.js
+++ Assets/Scripts/startup.js
@@ -1,22 +1,36 @@
 #pragma strict
+import System.IO;
 
 var station : GameObject;
+var map = "\\Resources\\coordinate.txt";
 
 function Start () {
 	//Test creation of a single testing station
-	var position = [151,0,9];
-	createStation(position);
+	var positions = ReadMap(map);
+	for (position in positions) {
+		createStation(position.Split(","[0]));
+	}
+	//createStation(position);
 }
 
-function createStation (position : Array) {
-	var x = position[0];
-	var y = position[1];
-	var z = position[2];
-	var clone : GameObject;
-	clone =  Instantiate(station, Vector3 (x, y, z), Quaternion.identity);
+function createStation (position : System.String[]) {
+	Debug.Log(position[0]);
+	var x = parseInt(position[0].Replace("\r",""));
+	var y = parseInt(position[1].Replace("\r",""));
+	var z = parseInt(position[2].Replace("\r",""));
+	Instantiate(station, Vector3 (x, y, z), Quaternion.identity);
 	
 }
 
 function Update () {
 
+}
+
+function ReadMap(map : String) {
+    var sr = new StreamReader(Application.dataPath + "/" + map);
+    var fileContents = sr.ReadToEnd();
+    sr.Close();
+ 
+    var lines = fileContents.Split("\n"[0]);
+    return lines;
 }
