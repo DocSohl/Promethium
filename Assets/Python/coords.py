2b5dcdcaffe123f18819d09164ea68816708de89
Greg Lewis <ghlkvf@gmail.com>
2013-03-13 22:22:31 -0600

    JSON location script

--- Assets/Python/coords.py
+++ Assets/Python/coords.py
@@ -1,34 +1,30 @@
 from __future__ import division
 import random
 import math
-
+import json
 
 ##Creates a random map using a spherical coordinate system, converts to cartesian coordinates, and writes to file
 
 
-eff = open("coordinate.txt","w")
-
-x = random.random()
-
-coord = []
-
+eff = open("..\\Resources\\coordinate.txt","w")
 
 for i in range(0,250):
-	b = []
+	
 	r = random.random()*250 + 75
 	theta = random.random()*2*math.pi
-	phi = random.random()*math.pi
-	b.append(int(r*math.sin(phi)*math.cos(theta)))
-	b.append(int(r*math.sin(phi)(math.sin(theta)))
-	b.append(int(r*math.cos(phi)))
-	coord.append(tuple(b))
-
-coord = set(coord)
-
-for line in coord:
+	phi = random.random()*math.pi	
 	
-	eff.write(str(line[0]) + "," + str(line[1]) + "," + str(line[2]) + "\n")
+	x = (r*math.sin(phi)*(math.cos(theta)))
+	y = (r*math.sin(phi)*(math.sin(theta)))
+	z = (r*math.cos(phi))
 	
-eff.close
+	data = {'x': x, 'y': y, 'z': z, 'station':i}
+	eff.write(json.dumps(data) + "\n")
+	
+eff.close()
+
+
+
+
 
 
